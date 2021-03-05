004d37f10cacd33a8ebcd4b554087c0fec3581f7
TroJIJI <adisrahimov0301@gmail.com>
2021-03-06 01:50:26 +0600

    hw3

--- src/com/geektech/Main.java
+++ src/com/geektech/Main.java
@@ -6,25 +6,21 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        double mas1[] = {10.3, 11.2, -2.3, -4.4, 4.7, 5.3, -4.4, 1.3, 4.7, 3.5, -2.4, -6.7, 4.5};
+        double mas1[] = {10.3, -11.2, 2.3, -4.4, 4.7, 5.3, -4.4, 1.3, 4.7, };
         double sum = 0;
-        int counter = 0;
-        for (int i = 0; i < mas1.length; i++) {
-            if (mas1[i] < 0) {
-                System.out.println("Первое отрицательное число = " + mas1[i]);
-                break;
-            }
-
-        }
-        for (int i = 2; i < mas1.length; i++) {
-            if (mas1[i] > 0) {
-                sum = sum + mas1[i];
+        double counter = 0;
+        boolean isFirstNegativeNumber = false;
+        for (double num : mas1) {
+            if (num < 0) {
+                isFirstNegativeNumber = true;
+            } else if (num > 0 && isFirstNegativeNumber) {
+                sum+=num;
                 counter++;
             }
-        }
-        System.out.println("Положительные числа после первого отрицательного - " + counter);
-        System.out.println("Среднее арифметическое " + sum / counter);
 
+        }
+        System.out.println("Количество положительных чисел после первого отрицательного - " + counter);
+        System.out.println("Среднее арифметическое " + sum / counter);
 
 
     }
